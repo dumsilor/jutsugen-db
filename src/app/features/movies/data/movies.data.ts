7e3d8b2dd45283187b937a99e83939547a403494
Mosiur <dum.silor@gmail.com>
2025-01-11 22:43:38 +0600

    added the horror movie links

--- src/app/features/movies/data/movies.data.ts
+++ src/app/features/movies/data/movies.data.ts
@@ -4,5 +4,53 @@ export const movie_links_horror = [
     },
     {
         link: 'https://www.facebook.com/share/14X1vNZiDf/'
-    }
+    },
+    {
+        link: 'https://www.facebook.com/share/1EoJ3m86EF/'
+    },
+    {
+        link: 'https://www.facebook.com/share/p/1H2SzWoWtn/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/1AqiKb5ceP/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/1CgsSMaAjp/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/1BJimUFcs6/',
+    },
+    {
+        link: 'https://www.facebook.com/share/152dbQPmWi/',
+    },
+    {
+        link: 'https://www.facebook.com/share/1PVU6uXzBu/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/18bMQjoDtc/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/14xkajkaVb/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/1AW3gbGdEY/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/18V6kQ3BBQ/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/1B5apu5f98/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/15fxxjR9mP/',
+    },
+    {
+        link: 'https://www.facebook.com/share/1WtsdNMCDC/',
+    },
+    {
+        link: 'https://www.facebook.com/share/1Bqg1TSHiz/',
+    },
+    {
+        link: 'https://www.facebook.com/share/p/18RTP48NWN/',
+    },
 ];
